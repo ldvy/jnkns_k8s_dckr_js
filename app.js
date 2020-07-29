0c900ead27ec467f5fc75b374487961e8b76de71
ldvy <hardway.studio@gmail.com>
2020-07-30 01:34:38 +0300

    Added simple serving with Express

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.use(express.static('static'))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', 'index.html'));
+});
+
+app.listen(8080);
